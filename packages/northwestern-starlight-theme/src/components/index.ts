7a45535de32e09fba6f347469a3a4ecdbfe2fb25
Danny Foster <danny@northwestern.edu>
2026-03-26 13:58:08 -0500

    feat: export components via barrel and package.json
    Add ./components export to package.json pointing to barrel file.
    Export PropertyTable, Property, PropertyGroup, Expandable, Tooltip,
    Glossary, Term, and Kbd from the barrel.

---
+++ packages/northwestern-starlight-theme/src/components/index.ts
@@ -0,0 +1,8 @@
+export { default as Expandable } from "./Expandable.astro";
+export { default as Glossary } from "./Glossary.astro";
+export { default as Kbd } from "./Kbd.astro";
+export { default as Property } from "./Property.astro";
+export { default as PropertyGroup } from "./PropertyGroup.astro";
+export { default as PropertyTable } from "./PropertyTable.astro";
+export { default as Term } from "./Term.astro";
+export { default as Tooltip } from "./Tooltip.astro";
